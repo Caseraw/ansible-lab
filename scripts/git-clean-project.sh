86fa6ca0c6746b01ed10ba679ba0bfba50a4a68b
Kasra <kasra.amirsarvari@gmail.com>
2021-09-25 11:59:05 +0200

    Update git-clean-project.sh

--- scripts/git-clean-project.sh
+++ scripts/git-clean-project.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 echo "Remove ~/ansible AND Clone new project"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
