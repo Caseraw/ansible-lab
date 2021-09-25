a0dbd9f3ada60e4bba67dbd33d92094a010a05a4
Kasra <kasra.amirsarvari@gmail.com>
2021-09-25 11:35:19 +0200

    Update git-pull-main.sh

--- scripts/git-pull-main.sh
+++ scripts/git-pull-main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "Pull latest master branch and rebase"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 cd ~/ansible
 
 git pull origin main --rebase
