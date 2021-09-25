3b3ec07e7d4528b0e0009878f7d0bf84012f88fb
Kasra <kasra.amirsarvari@gmail.com>
2021-09-25 11:50:39 +0200

    Update prep-lab.sh

--- scripts/prep-lab.sh
+++ scripts/prep-lab.sh
@@ -4,28 +4,28 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "${GREEN}Install Python${NC}"
+echo -e "${GREEN}Install Python${NC}"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 sudo dnf module install python39 -y
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "${GREEN}Install additional system packages${NC}"
+echo -e "${GREEN}Install additional system packages${NC}"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 sudo dnf install git tree lsof bash-completion -y &&\
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "${GREEN}Show python details${NC}"
+echo -e "${GREEN}Show python details${NC}"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 sudo alternatives --list
 echo ""
 sudo python3 --version
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "${GREEN}Install pip packages${NC}"
+echo -e "${GREEN}Install pip packages${NC}"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 python3 -m pip install --user --upgrade pip setuptools ansible
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "${GREEN}Show ansible details${NC}"
+echo -e "${GREEN}Show ansible details${NC}"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 ansible --version
