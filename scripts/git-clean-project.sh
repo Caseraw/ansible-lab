0de42c1fde40abc7fed36e0fdb6d87f7fe155c63
Kasra <kasra.amirsarvari@gmail.com>
2021-09-25 14:37:58 +0200

    Update git-clean-project.sh

--- scripts/git-clean-project.sh
+++ scripts/git-clean-project.sh
@@ -8,3 +8,10 @@ printf "${GREEN}Remove ~/ansible AND Clone new project${NC}\n"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 rm -rf ~/ansible
 git clone https://github.com/Caseraw/ansible-lab.git ~/ansible
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+printf "${GREEN}Install project dependencies${NC}\n"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+cd ~/ansible
+ansible-galaxy role install -r requirements.yml
+cd
