3e8bcdc8a7a0fb016923c82ad5ba2f6c6b153553
Kasra <kasra.amirsarvari@gmail.com>
2021-09-25 13:53:59 +0200

    Update git-clean-project.sh

--- scripts/git-clean-project.sh
+++ scripts/git-clean-project.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Remove ~/ansible AND Clone new project"
+printf "${GREEN}Remove ~/ansible AND Clone new project${NC}\n"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 rm -rf ~/ansible
 git clone https://github.com/Caseraw/ansible-lab.git ~/ansible
